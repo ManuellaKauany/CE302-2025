d773c6bd9646acd6292accebb2a578f896d7838a
ManuellaKauany <manuellakauany@ufpr.br>
2025-08-27 17:12:41 -0300

    Materia sobre Terminal

---
+++ Modulo 1- Terminal.R
@@ -0,0 +1,43 @@
+# Modulo 1
+## Unix, Git & Markdown
+
+# Terminal
+## O que é um Terminal/Console/Shell?
+É uma interface de linha de comando que permite interagir com o sistema operacional usando texto. Em vez de clicar em ícones, você digita comandos.
+
+## Por que deveriamos usar?
+Reprodutibilidade: Comandos podem ser salvos em scripts para refazer cada passo do seu trabalho.
+Eficiência: Gerencie arquivos, pastas e projetos complexos de forma muito mais rápida.
+Ferramentas Avançadas: Muitas ferramentas de modelagem e computação de alta performance só funcionam via linha de comando
+
+## Principais códigos
+pwd Mostra em qual pasta você está agora
+
+man Mostra o manual de um comando
+
+ls	Lista os arquivos e pastas no diretório atual
+
+cd	Muda de diretório
+
+mkdir Cria uma nova pasta
+
+mv  Move ou renomeia um arquivo/pasta
+
+cp Copia um arquivo/pasta
+
+rm Apaga um arquivo. CUIDADO: não tem lixeira!
+
+rm -r	Apaga uma pasta e todo o seu conteúdo
+
+cat	Mostra o conteúdo de um arquivo
+
+grep  Busca por padrões em arquivos
+
+find	Busca por arquivos e pastas
+
+history  Mostra o histórico de comandos usados
+
+clear 	Limpa a tela do terminal.	clear
+
+echo    Imprime texto na tela
+[ video aula, terminal](https://youtu.be/bRuiUMK6sAc)
