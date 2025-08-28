1b271b724afbe4ad85d2360f7fbb4e013e7b0afc
ManuellaKauany <manuellakauany@ufpr.br>
2025-08-27 23:41:40 -0300

    tentativa de commit materia sobre terminal

---
+++ Módulo 1/Terminal.R
@@ -0,0 +1,49 @@
+# Modulo 1
+## Unix, Git & Markdown
+
+# Terminal
+
+## O que é um Terminal/Console/Shell?
+
+É uma interface de linha de comando que permite interagir com o sistema operacional usando texto. Em vez de clicar em ícones, você digita comandos.
+
+## Por que deveriamos usar?
+
+Reprodutibilidade: Comandos podem ser salvos em scripts para refazer cada passo do seu trabalho.
+Eficiência: Gerencie arquivos, pastas e projetos complexos de forma muito mais rápida.
+Ferramentas Avançadas: Muitas ferramentas de modelagem e computação de alta performance só funcionam via linha de comando
+
+## Principais códigos
+
+pwd	Mostra em qual pasta você está agora.	
+
+man Mostra o manual de um comando.
+
+ls Lista os arquivos e pastas no diretório atual.
+
+cd Muda de diretório.
+
+mkdir	Cria uma nova pasta.	
+
+mv Move ou renomeia um arquivo/pasta.
+
+cp Copia um arquivo/pasta.
+
+rm Apaga um arquivo. CUIDADO: não tem lixeira
+
+rm -r Apaga uma pasta e todo o seu conteúdo.
+
+cat Mostra o conteúdo de um arquivo.
+
+grep	Global Regular Expression Print: Busca por padrões em arquivos.	grep "erro" log.txt
+
+find	Busca por arquivos e pastas.	find . -name "*.csv"
+
+history	Mostra o histórico de comandos usados.	history
+
+clear	Limpa a tela do terminal.	clear
+
+echo     Imprime texto na tela
+
+[ video aula, terminal](https://youtu.be/bRuiUMK6sAc)
+
